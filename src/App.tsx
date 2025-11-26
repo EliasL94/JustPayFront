185f261136c327e0fbee0d804bcfadb7c0569808
Matheo-sys <matheosoriano.pro@gmail.com>
2025-11-26 09:54:24 +0100

    Connexion Backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState('Chargement...');
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(() => {
+    fetch('/users/bonjour')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Erreur HTTP: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        setMessage(data.message); 
+      })
+      .catch(error => {
+        console.error('Erreur lors de la récupération des données:', error);
+        setMessage('Erreur de connexion à l\'API.');
+      });
+  }, []);
+
+  return <h1>Message de FastAPI: {message}</h1>;
 }
 
 export default App
